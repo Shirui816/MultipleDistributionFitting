506f364d20831b197cd8bd5bbd8f6531e52d0e0a
施呵呵 <shirui816@gmail.com>
2018-04-01 18:49:09 +0800

    Add least square fitting module
    Add FitLSQ
    Bug fix and pep8 check for MultiFuncs.

---
+++ FitLSQ.py
@@ -0,0 +1,101 @@
+from scipy.optimize import curve_fit
+import numpy as np
+import inspect
+import warnings
+
+
+class FitLSQ(object):
+    r"""Least Square fitting model."""
+
+    def __init__(self, func):
+        r"""Init function.
+
+        Arguments:
+        func: callable, function to be fitted
+        """
+        self.Parameters = None
+        self.Covariances = None
+        self.InitParameters = None
+        self.Bounds = None
+        self.Function = func
+        self._N, self._P = self.__get_paramter()
+
+    def __get_paramter(self):
+        r"""Get number of parameters.
+
+        Returns: tuple, (n_parameters, parameters)
+        """
+        args = inspect.getfullargspec(self.Function).args
+        # -1 for 'x' is always the 1st value.
+        return len(args) - 1, args
+
+    def set_bounds(self, bounds, known=[]):
+        r"""Set bounds for target function.
+
+        Arguments:
+        bounds: 2d-list for lower and upper bounds (lb, ub) for arguments
+                of base function. +/-np.inf for no bounds.
+        n: number of parameters ofl BASE functions.
+        known: Known parts in functions.
+        Returns:
+        self
+        """
+        warnings.warn("Bounds must EXACTLY match the base function!",
+                      UserWarning)
+        lb, ub = bounds
+        # number of bounds must equal to the base function n_parameters' number
+        N = self._N + len(known) - known.count(None)
+        NBases = N // len(lb)
+        Unknow = NBases - len(known) // len(lb)
+        assert isinstance(bounds, list), "Bounds must be list object."
+        assert len(ub) == len(lb) and N % len(ub) == 0,\
+            "Number of bounds must equal to number of BASE's arguments!"
+        _lb = [lb[_ % len(lb)]
+               for _ in range(len(known)) if known[_] is None] + lb * Unknow
+        _ub = [ub[_ % len(ub)]
+               for _ in range(len(known)) if known[_] is None] + ub * Unknow
+        self.Bounds = [_lb, _ub]
+        return self
+
+    def set_p0(self, p0, known=[]):
+        r"""Set initial values for fitting.
+
+        Arguments:
+        p0: tuple or list for initial parameters.
+        known: list for known components.
+
+        Returns:
+        self
+        """
+        warnings.warn("p0 must EXACTLY match the base function!",
+                      UserWarning)
+        N = self._N + len(known) - known.count(None)
+        NBases = N // len(p0)
+        Unknow = NBases - len(known) // len(p0)
+        assert isinstance(p0, list), "p0 must be list object."
+        # number of bounds must equal to the base function n_parameters' number
+        assert N % len(p0) == 0,\
+            "Number of initials must equal to number of BASE arguments!"
+        _p0 = [p0[_ % len(p0)]
+               for _ in range(len(known)) if known[_] is None] + p0 * Unknow
+        self.InitParameters = _p0
+        return self
+
+    def fit(self, x, y, **kwargs):
+        r"""Fit the model.
+
+        Arguments:
+        x: np.array for x
+        y: np.array for y
+
+        Keyword Arguments:
+        kwargs that fits scipy.optimize.curve_fit
+
+        Returns:
+        self
+        """
+        p0 = self.InitParameters or None
+        bounds = self.Bounds or (-np.inf, np.inf)
+        self.Parameters, self.Covariances =\
+            curve_fit(self.Function, x, y, p0=p0, bounds=bounds, **kwargs)
+        return self
--- MultiFuncs.py
+++ MultiFuncs.py
@@ -5,26 +5,32 @@ import numpy as np
 
 
 def n_func(n, known=[]):
-    '''Decorator for defining functions mixed by base function. For scipy.optimize.curv_fit
+    r"""Decorator for defining functions mixed by base function.
+
+    For scipy.optimize.curv_fit.
+
     Arguments:
     n: numbers of functions
     Keyword arguments:
-    known: optional, known componets of functions, n_{knowns} \times n_{function parameters}
+    known: optional, known componets of functions,
+            n_{knowns} \times n_{function parameters}
 
     Returns: Function that mixed n base functions with known parameters.
-    '''
+    """
     def _wrapper(func):
         signature_ = inspect.getfullargspec(func).args
         # For scipy fitting
         assert signature_[
             0] == 'x', "The function must has `x' for its 1st value!"
-        # make sure that functions like f(x) appear.... what would you want to fit?
-        assert len(signature_[0]) > 1, "........Seriously?"
+        # make sure that functions like f(x) appear...
+        # what would you want to fit?
+        assert len(signature_) > 1, "........Seriously?"
         f_args = signature_[1:]
         Nf, Nk = len(f_args), len(known)
-        assert Nk % Nf == 0, "Paramerters of known components must equal to target function!"
-        assert len(known) <= n * \
-            Nf, "Number of known parts must lesser than number of functions!"
+        assert Nk % Nf == 0,\
+            "Paramerters of known components must equal to target function!"
+        assert len(known) <= n * Nf,\
+            "Number of known parts must lesser than number of functions!"
         if not (len(known) != n * Nf or None in known):
             # I am sure that no parameters are put for fitting
             raise ValueError("Seriously?")
@@ -39,15 +45,19 @@ def n_func(n, known=[]):
             'x', kind=inspect.Parameter.POSITIONAL_OR_KEYWORD)]
         _signature = _signature + \
             [inspect.Parameter(
-                _, kind=inspect.Parameter.POSITIONAL_OR_KEYWORD) for _ in nf_args]
+                _, kind=inspect.Parameter.POSITIONAL_OR_KEYWORD)
+                for _ in nf_args]
 
         def __wrapper(x, *args):
             assert len(args) == len(
                 nf_args), "Number of args must equal to the unknowns!"
             args = list(args)
-            # this is amazing [ None, a, b, None ] + [c,d] -> [c,a,b,d], a fancy substitution
+            # this is amazing
+            # [ None, a, b, None ] + [c,d] -> [c,a,b,d], a fancy substitution
             variables = [args.pop(0) if _ is None else _ for _ in known] + args
-            return np.sum([func(x, *variables[_:_ + Nf]) for _ in range(0, len(variables), Nf)], axis=0)
+            return np.sum([func(x, *variables[_:_ + Nf])
+                           for _ in range(0, len(variables), Nf)], axis=0)
+
         __wrapper.__signature__ = inspect.Signature(parameters=_signature)
         return __wrapper
     return _wrapper
@@ -55,14 +65,28 @@ def n_func(n, known=[]):
 
 # maker
 def n_func_maker(n, func, known=[]):
+    r"""Maker for defining functions mixed by base function.
+
+    For scipy.optimize.curv_fit.
+
+    Arguments:
+    n: numbers of functions
+    func: callable, base function
+    Keyword arguments:
+    known: optional, known componets of functions,
+            n_{knowns} \times n_{function parameters}
+
+    Returns: Function that mixed n base functions with known parameters.
+    """
     signature_ = inspect.getfullargspec(func).args
     assert signature_[0] == 'x', "The function must has `x' for its 1st value!"
-    assert len(signature_[0]) > 1, "........Seriously?"
+    assert len(signature_) > 1, "........Seriously?"
     f_args = signature_[1:]
     Nf, Nk = len(f_args), len(known)
-    assert Nk % Nf == 0, "Paramerters of known components must equal to target function!"
-    assert len(known) <= n * \
-        Nf, "Number of known parts must lesser than number of functions!"
+    assert Nk % Nf == 0,\
+        "Paramerters of known components must equal to target function!"
+    assert len(known) <= n * Nf,\
+        "Number of known parts must lesser than number of functions!"
     nf_args = [f_args[_ % Nf] + str(_ // Nf + Nk // Nf)
                for _ in range(n * Nf - Nk)]
     nf_args = [f_args[_ % Nf] + str(_ // Nf)
@@ -79,16 +103,35 @@ def n_func_maker(n, func, known=[]):
             nf_args), "Number of args must equal to the unknowns!"
         args = list(args)
         variables = [args.pop(0) if _ is None else _ for _ in known] + args
-        return np.sum([func(x, *variables[_:_ + Nf]) for _ in range(0, len(variables), Nf)], axis=0)
+        return np.sum([func(x, *variables[_:_ + Nf])
+                       for _ in range(0, len(variables), Nf)], axis=0)
+
     _func.__signature__ = inspect.Signature(parameters=_signature)
     return _func
 
 
 # addr
-def flatten(l): return [item for sublist in l for item in sublist]
+def flatten(l):
+    r"""Flatten 2d list to 1d.
+
+    Arguments:
+    l:  2D list
+    Returns: flattened list
+    """
+    return [item for sublist in l for item in sublist]
 
 
 def n_func_mix(funcs):
+    r"""Mixer for defining functions mixed by base function.
+
+    For scipy.optimize.curv_fit.
+
+    Arguments:
+    funcs: A list of callables
+    func: callable, base function
+
+    Returns: Function that mixed n base functions.
+    """
     signature_ = [inspect.getfullargspec(_).args for _ in funcs]
     lengths = [len(_) - 1 for _ in signature_]
     assert 0 not in lengths, "........Seriously?"
@@ -97,7 +140,7 @@ def n_func_mix(funcs):
         'x'], "All functions must have `x' for their 1st parameters!"
     _p = np.cumsum([0] + lengths).astype(np.int)
     signature_ = [__ + str(_) for _ in range(len(funcs))
-                  for __ in signature_[_] if not 'x' in __]
+                  for __ in signature_[_] if 'x' not in __]
     _signature = [inspect.Parameter(
         'x', kind=inspect.Parameter.POSITIONAL_OR_KEYWORD)]
     _signature = _signature + \
@@ -107,6 +150,8 @@ def n_func_mix(funcs):
     def _func(x, *args):
         assert len(args) == len(_signature) - \
             1, "Number of args must equal to the unknowns!"
-        return np.sum([funcs[_](x, *args[_p[_]:_p[_ + 1]]) for _ in range(0, len(_p) - 1)], axis=0)
+        return np.sum([funcs[_](x, *args[_p[_]:_p[_ + 1]])
+                       for _ in range(0, len(_p) - 1)], axis=0)
+
     _func.__signature__ = inspect.Signature(parameters=_signature)
     return _func
